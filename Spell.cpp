5e64add476f64b31e0375aede0a8c4095b0b73b9
ClassyDragon <cgandeg@gmail.com>
2020-07-29 10:50:11 -0400

    Revamped Spell class to be split into static and dynamic attributes, bullet lifetime based on a set duration instead of being at a certain location

--- Spell.cpp
+++ Spell.cpp
@@ -1,92 +1,123 @@
 #include "Spell.hpp"
 
-//#define PI 3.141596
-const float speed = 2.f;
-const int numBullet = 16;
-
-// Movement Functions:
-void mov_linear(th::Bullet& bullet) {
-    bullet.position.x += bullet.direction.x;
-    bullet.position.y += bullet.direction.y;
+ga::Vector2<float> homing_dir(const ga::v2f& spawn, const ga::v2f& target) {
+    ga::Vector2<float> v;
+    v.x = target.x - spawn.x;
+    v.y = target.y - spawn.y;
+    return v.unitVector();
 }
-
-void mov_whirlpool(th::Bullet& bullet) {
-    bullet.rotation.angle += 0.2;
-    bullet.position.x = bullet.center.x + ((bullet.lifetime * bullet.speed) * cos((bullet.rotation.angle) * PI / 180));
-    bullet.position.y = bullet.center.y + ((bullet.lifetime * bullet.speed) * sin((bullet.rotation.angle) * PI / 180));
+namespace th {
+    void Bullet::Move_Radial() {
+        this->v_Position.x += this->v_Direction.x;
+        this->v_Position.y += this->v_Direction.y;
+    }
+    void Bullet::Move_Whirlpool() {
+        this->f_Rotation += 0.2;
+        this->v_Position.x = this->v_Center.x + ((this->n_Lifetime * this->f_Speed) * cos((this->f_Rotation) * PI / 180));
+        this->v_Position.y = this->v_Center.y + ((this->n_Lifetime * this->f_Speed) * sin((this->f_Rotation) * PI / 180));
+    }
+    void Bullet::Move_Homing() {
+        this->v_Position.x += this->v_Direction.x * this->f_Speed;
+        this->v_Position.y += this->v_Direction.y * this->f_Speed;
+    }
 }
 
 namespace th {
+    Spell::Spell(const C_SpellParams& csp, const D_SpellParams& dsp, ga::Window* windowPointer) {
+        this->sprite = csp.sprite;
+        this->n_Formation = csp.n_Formation;
+        this->n_NumBullets = csp.n_NumBullets;
+        this->n_Lifetime = 0;
+        this->n_Duration = csp.n_Duration;
+        this->f_Speed = csp.f_Speed;
+        this->v_Position = dsp.v_SpellPosition;
+        this->windowPointer = windowPointer;
 
-    Spell::Spell(SpellInfo& si, const ga::Position2D<float>& position, const int& rotation)
-        : formation(si.formation), lifetime(0), empty(false), sprite(si.sprite)
-    {
-        this->sprite = si.sprite;
-        float angle = 360.0f / si.numBullets;
-        this->position.x = position.x;
-        this->position.y = position.y;
-        switch (si.formation) {
-            case (int)FORMATION::RADIAL:
-                for (float i = 0; i < 360.0f; i += angle) {
-                    switch (si.startingPosition) {
-                        case (int)POSITION::OWNER:
-                            this->bullets.emplace_back(Bullet {
-                                .position = ga::Position2D<float>(cos((i + rotation) * PI / 180) + position.x, sin((i + rotation) * PI / 180) + position.y),
-                                .direction = ga::Vector2<float>(cos((i + rotation) * PI / 180) * si.speed, sin((i + rotation) * PI / 180) * si.speed),
-                                .rotation = ga::Rotation2D(i + rotation),
-                                .coll = ga::Collider(ga::ColliderOpt{ .width = 10.f, .height = 10.f }),
-                                .OOB = false,
-                                .lifetime = 0
-                            });
-                            this->bullets.back().coll.setRotation(bullets.back().rotation);
-                            break;
-                        case (int)POSITION::CUSTOM:
-                            break;
-                    }
-                }
+        switch (this->n_Formation) {
+            case (int)FORM::RAD:
+                initRadial(csp, dsp);
                 break;
-            case (int)FORMATION::WHIRLPOOL:
-                for (float i = 0.f; i < 360.0f; i += angle) {
-                    this->bullets.emplace_back(Bullet {
-                        .position = ga::Position2D<float>(cos((i + rotation) * PI / 180) + position.x, sin((i + rotation) * PI / 180) + position.y),
-                        .center = ga::Position2D<float>(this->position.x, this->position.y),
-                        .speed = si.speed,
-                        .rotation = ga::Rotation2D(i + rotation),
-                        .coll = ga::Collider(ga::ColliderOpt{ .width = 10.f, .height = 10.f }),
-                        .OOB = false,
-                        .lifetime = 0
-                    });
-                    this->bullets.back().coll.setRotation(bullets.back().rotation);
-                }
+            case (int)FORM::WHIRL:
+                initWhirlpool(csp, dsp);
+                break;
+            case (int)FORM::HOMING:
+                initHoming(csp, dsp);
                 break;
         }
     }
 
-    void Spell::update() {
-        this->lifetime++;
-        bullets.remove_if([&](Bullet& bullet) {
-            // UPDATE BULLETS HERE
-            bullet.lifetime++;
-            switch (this->formation) {
-                case (int)FORMATION::RADIAL:
-                    mov_linear(bullet);
-                    break;
-                case (int)FORMATION::WHIRLPOOL:
-                    mov_whirlpool(bullet);
-                    break;
-                default: break;
-            }
-            bullet.coll.setPosition(bullet.position);
-            return bullet.OOB;
+    void Spell::initRadial(const C_SpellParams& csp, const D_SpellParams& dsp) {
+        float angle = 360.0f / n_NumBullets;
+        for (float i = 0; i < 360.0f; i += angle) {
+            this->l_Bullets.emplace_back(Bullet{
+                .v_Position  = { cos(i * PI / 180) + v_Position.x, sin(i * PI / 180) + v_Position.y },
+                .v_Direction = { cos(i * PI / 180) * f_Speed, sin(i * PI / 180) * f_Speed },
+                .n_Duration = this->n_Duration,
+                .f_Rotation = i,
+                .f_Speed = this->f_Speed,
+                .c_Collider = { ga::ColliderOpt{ .width = 10.f, .height = 10.f } }
+            });
+            this->l_Bullets.back().c_Collider.setRotation(l_Bullets.back().f_Rotation);
+        }
+    }
+
+    void Spell::initWhirlpool(const C_SpellParams& csp, const D_SpellParams& dsp) {
+        float angle = 360.0f / n_NumBullets;
+        for (float i = 0.f; i < 360.0f; i += angle) {
+            this->l_Bullets.emplace_back(Bullet {
+                .v_Position = { cos((i) * PI / 180) + v_Position.x, sin((i) * PI / 180) + v_Position.y },
+                .v_Center = { this->v_Position.x, this->v_Position.y },
+                .n_Duration = this->n_Duration,
+                .f_Rotation = i,
+                .f_Speed = this->f_Speed,
+                .c_Collider = ga::Collider(ga::ColliderOpt{ .width = 10.f, .height = 10.f }),
+            });
+            this->l_Bullets.back().c_Collider.setRotation(l_Bullets.back().f_Rotation);
+        }
+    }
+
+    void Spell::initHoming(const C_SpellParams& csp, const D_SpellParams& dsp) {
+        this->l_Bullets.emplace_back(Bullet {
+            .v_Position = { v_Position.x, v_Position.y },
+            .v_Direction = homing_dir({v_Position.x, v_Position.y}, dsp.v_PlayerPosition),
+            .v_Target = dsp.v_PlayerPosition,
+            .n_Duration = this->n_Duration,
+            .f_Rotation = 0,
+            .f_Speed = this->f_Speed,
+            .c_Collider = { ga::ColliderOpt{ .width = 10.f, .height = 10.f } },
         });
-        this->empty = bullets.empty();
+        this->l_Bullets.back().f_Rotation = this->l_Bullets.back().v_Direction.unitVector().angle();
+        this->l_Bullets.back().c_Collider.setRotation(l_Bullets.back().f_Rotation);
     }
 
     void Spell::render(ga::Window& window) {
-        for (auto& bullet : bullets) {
-            this->sprite->setPosition(ga::Position2D<float>(bullet.position.x, bullet.position.y));
-            this->sprite->setRotation(bullet.rotation);
+        for (auto& bullet : l_Bullets) {
+            this->sprite->setPosition(ga::Position2D<float>(bullet.v_Position.x, bullet.v_Position.y));
+            this->sprite->setRotation(bullet.f_Rotation);
             window.getRenderer().Draw(*sprite);
         }
+    }
+    
+    void Spell::update() {
+        this->n_Lifetime++;
+        this->l_Bullets.remove_if([&](Bullet& bullet) {
+            // UPDATE BULLETS HERE
+            bullet.n_Lifetime++;
+            switch (this->n_Formation) {
+                case (int)FORM::RAD:
+                    bullet.Move_Radial();
+                    break;
+                case (int)FORM::WHIRL:
+                    bullet.Move_Whirlpool();
+                    break;
+                case (int)FORM::HOMING:
+                    bullet.Move_Homing();
+                    break;
+                default: break;
+            }
+            bullet.c_Collider.setPosition(ga::Position2D<float>( bullet.v_Position.x, bullet.v_Position.y ));
+            return bullet.n_Lifetime > bullet.n_Duration;
+        });
+        this->b_Empty = this->l_Bullets.empty();
     }
 }
--- Spell.hpp
+++ Spell.hpp
@@ -1,8 +1,22 @@
 #pragma once
 #include "pch.h"
-#include <GaiaGL/Graphics.h>
-#include <GaiaGL/System.h>
+#include <GaiaGL/Graphics.hpp>
+#include <GaiaGL/System.hpp>
 
+/*
+ * Constructing a spell can be broken down into two sets of parameters:
+ * 1. Static and constant parameters
+ * 2. Dynamic parameters
+ * ============================================================================
+ * Static Parameters are things that will be the same for every copy of a
+ * particular spell, such as the formation, speed, number of bullets, etc.
+ * 
+ * Dynamic Parameters are things that will be potentially different for each
+ * copy of a spell, such as the target (player) position
+ * ============================================================================
+ */
+
+/*
 namespace th {
 
     enum class POSITION {
@@ -10,7 +24,7 @@ namespace th {
     };
 
     enum class FORMATION {
-        NONE = 0, RADIAL, WHIRLPOOL
+        NONE = 0, RADIAL, WHIRLPOOL, HOMING, RAD2
     };
 
     struct SpellInfo {
@@ -21,21 +35,28 @@ namespace th {
         int numBullets;
         float speed;
         ga::Sprite* sprite;
+        ga::Position2D<float> playerPosition;
     };
 
     struct Bullet {
         ga::Position2D<float> position;
         union {
-            ga::Position2D<float> center;
+            ga::Position2D<float> center; // important for whirlpool
+            ga::Position2D<float> target; // important for homing
         };
         union {
             ga::Vector2<float> direction;
-            float speed;
         };
+        float speed;
         ga::Rotation2D rotation;
         ga::Collider coll;
         bool OOB;
         int lifetime = 0;
+
+        // Movement functions
+        void mov_radial();
+        void mov_whirlpool();
+        void mov_homing();
     };
 
     class Spell {
@@ -52,4 +73,75 @@ namespace th {
         bool empty;
     };
 
+}
+*/
+
+namespace th {
+    enum class FORM {
+        NONE = 0,
+        // Simple Spell formations
+        RAD, // radial
+        WHIRL, // whirlpool
+        HOMING,
+
+        // More complex formations
+    };
+
+    class Bullet {
+        public:
+        ga::v2f v_Position;
+        ga::v2f v_Direction;
+        union {
+            ga::v2f v_Center;
+            ga::v2f v_Target;
+        };
+        int n_Duration;
+        float f_Rotation;
+        float f_Speed;
+        ga::Collider c_Collider;
+
+        // Static
+        int n_Lifetime = 0;
+
+        void Move_Radial();
+        void Move_Whirlpool();
+        void Move_Homing();
+    };
+
+    struct C_SpellParams {
+        int n_Formation;
+        int n_NumBullets;
+        int n_Duration;
+        float f_Speed;
+        ga::Sprite* sprite;
+    };
+
+    struct D_SpellParams {
+        ga::v2f v_SpellPosition;
+        ga::v2f v_PlayerPosition;
+    };
+
+    class Spell {
+        public:
+        Spell() =delete;
+        Spell(const C_SpellParams& csp, const D_SpellParams& dsp, ga::Window* windowPointer);
+
+        void initRadial(const C_SpellParams& csp, const D_SpellParams& dsp);
+        void initWhirlpool(const C_SpellParams& csp, const D_SpellParams& dsp);
+        void initHoming(const C_SpellParams& csp, const D_SpellParams& dsp);
+
+        void render(ga::Window& window);
+        void update();
+
+        int n_Formation;
+        int n_NumBullets;
+        int n_Lifetime;
+        int n_Duration;
+        float f_Speed;
+        bool b_Empty;
+        ga::v2f v_Position;
+        std::list<Bullet> l_Bullets;
+        ga::Window* windowPointer;
+        ga::Sprite* sprite;
+    };
 }
